b8bcc6a15c7963f4b5cd34143d3f9ae3f55f2db1
Bassel Bakr <bassel.bakr@gmail.com>
2026-06-14 15:12:55 +0300

    refactor(#17): make stat date time length explicit

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
 pub const STAT_DATE_TIME_FORMAT: &str = "%Y.%m.%d-%H.%M.%S";
-pub const STAT_DATE_TIME_LEN: usize = STAT_DATE_TIME_FORMAT.len();
+pub const STAT_DATE_TIME_LEN: usize = 19;
 pub const STAT_FILE_SUFFIX: &str = " Stats.csv";
 
 pub const CONFIG_FILE: &str = "config.json";
